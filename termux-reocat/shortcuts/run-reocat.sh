195cacfa8c0898971a569c3045875d8b7cdb55d7
golargescale <songbo@gmail.com>
2024-01-05 22:54:41 -0800

    Create run-reocat.sh
    to be used with Termux-Widget

---
+++ termux-reocat/shortcuts/run-reocat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ***********************************************************
+# * Copyright (c) 2023 litemoment.com. All rights reserved. *
+# ***********************************************************
+# Contact: webmaster@litemoment.com
+
+cd ~/storage/dcim/SDUSB/termux-reocat
+bash reocat.sh
