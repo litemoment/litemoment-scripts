df0192a646981f34a859c80a28e18874415e822f
golargescale <songbo@gmail.com>
2024-03-10 23:54:08 -0700

    simple lite buttons
    shortcut lite button

---
+++ termux-reocat/dotshortcuts/Button-LITE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ***********************************************************
+# * Copyright (c) 2024 litemoment.com. All rights reserved. *
+# ***********************************************************
+# Contact: webmaster@litemoment.com
+
+curl -X POST https://rest.litemoment.com/events \
+--user "$(tr -d '\r\n' < ~/.liteaccount.txt)" \
+-H 'Content-Type: application/x-www-form-urlencoded;charset=utf-8' \
+-d 'eventType=LITE&eventTS=0&devTS=0&devSN=TERMUX001&batLevel=0&evtSource=0&evtCount=0'
